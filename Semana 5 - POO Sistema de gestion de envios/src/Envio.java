dc9d8df5e099a8683316c611c751831727aca43c
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-08-17 19:41:12 -0600

    Envo Internacional
    Se agrego los envíos Internacional y los cálculos.

--- Semana 5 - POO Sistema de gestion de envios/src/Envio.java
+++ Semana 5 - POO Sistema de gestion de envios/src/Envio.java
@@ -1,3 +1,4 @@
+import java.net.PortUnreachableException;
 import java.security.PublicKey;
 
 public class Envio {
@@ -103,5 +104,67 @@ public class EnvioNacional extends Envio {
                 MostrarResumen();
             }
         }
+
+        //ENVIO INTERNACIONAL
+
+        public Class EnvioInternacional extends Envio {
+
+            private String PaisDestino;
+
+            private final double CARGO_INTERNACIONAL = 75.00;
+            private final double PORCENTAJE_RECARGO = 0.12;
+
+            public EnvioInternacional(String Codigo, String Destinatario,
+            double Peso, StringPaisDestino){
+
+                super(Codigo, Destinatario, Peso);
+
+                this.PaisDestino = PaisDestino;
+            }
+
+            public String getPaisDestino () {
+                return PaisDestino;
+            }
+
+            public double CalcularRecargo () {
+                return CalcularCostoBase() * PORCENTAJE_RECARGO;
+            }
+
+            @Override
+            public double CalcularCostoFinal () {
+
+                return CalcularCostoBase()
+                        + CARGO INTERNACIONAL
+                        + CalcularRecargo();
+            }
+
+            @Override
+            public void mostrarResumen ( boolean desgloseCompleto){
+
+                if (desgloseCompleto) {
+
+                    System.out.println("\n========== ENVÍO INTERNACIONAL ==========");
+                    System.out.println("Código: " + getCodigo());
+                    System.out.println("Destinatario: " + getDestinatario());
+                    System.out.println("País de destino: " + PaisDestino);
+
+                    System.out.printf("Peso: %.2f kg%n", getPeso());
+                    System.out.printf("Costo base: Q%.2f%n",
+                            CalcularCostoBase());
+
+                    System.out.printf("Cargo internacional: Q%.2f%n",
+                            CARGO_INTERNACIONAL);
+
+                    System.out.printf("Recargo del 12%%: Q%.2f%n",
+                            CalcularRecargo());
+
+                    System.out.printf("Costo final: Q%.2f%n",
+                            CalcularCostoFinal());
+
+                } else {
+                    mostrarResumen();
+                }
+            }
+        }
+
         
-}
